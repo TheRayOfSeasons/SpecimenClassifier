66e84ef7ce616b6a43d75300b34edcaee5fc3c61
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-02-02 17:11:24 +0800

    Direction Image Upload and Jquery

---
+++ static/javascript/specimen-upload-photo.js
@@ -0,0 +1,66 @@
+const directions = ['north', 'east', 'west', 'south'];
+
+const buildImageId = (direction, number) =>
+{
+    return `${direction}-${number.toString()}`;
+}
+
+const buildCollectionSelector = (direction) =>
+{
+    return `#${direction}Images`;
+}
+
+const buildImageButtonSelector = (direction, number) =>
+{
+    return `#${direction}ImageButton-${number}`;
+}
+
+const generateNewImageButton = (direction, number) => 
+{
+    let i = number + 1;
+    let selector = buildImageButtonSelector(direction, i);
+    return `<input type="file" name="fileupload" value="fileupload" id="${selector}" onChange="displayUploaded(this, '${direction}', ${i})">`
+}
+
+const insertImage = (direction, number) =>
+{
+    const imageCollection = buildCollectionSelector(direction);
+    const generateImageElement = new Promise((resolve) => 
+    {
+        let i = number;
+        let imageID = buildImageId(direction, i);
+
+        resolve(
+            {
+                template: `<img id='${imageID}' src="#">`,
+                id: imageID,
+            }
+        );
+    });
+
+    return generateImageElement.then((newImage) => {
+        $(imageCollection).prepend(newImage.template);
+        $(imageCollection).append(generateNewImageButton(direction, number))
+        return '#' + newImage.id;
+    });
+}
+
+const displayUploaded = (input, direction, number) => 
+{
+    insertImage(direction, number).then((id) => {
+        if (input.files && input.files[0]) 
+        {
+            var reader = new FileReader();
+
+            reader.onload = (e) => 
+            {
+                $(id)
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    });
+}
